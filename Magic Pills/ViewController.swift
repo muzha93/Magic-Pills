41029d7f8e0df4184a8d0cf78422494903348bef
muzha93 <luka@lukas-MacBook-Pro.local>
2016-10-10 21:46:42 +0200

    first commit

--- Magic Pills/ViewController.swift
+++ Magic Pills/ViewController.swift
@@ -8,10 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate{
 
+    
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    @IBOutlet weak var buyNowBtn: UIButton!
+    
+    let states = ["Alaska", "Alabama", "Croatia", "Bulgaria", "New York"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +30,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stateBtnPressed(_ sender: AnyObject) {
+        statePicker.isHidden = false
+//       countryLabel.isHidden = true
+//        countryTextField.isHidden = true
+//        zipCode.isHidden = true
+//        zipCodeTextField.isHidden = true
+    }
+    
+    @IBAction func buyNowBtnPressed(_ sender: AnyObject) {
+        
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+    }
 
 }
 
